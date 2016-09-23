d2bfc83dc2d95575b992b4b27a01796406188022
OmGiTzJake <jake@jakekarasik.com>
2016-09-22 21:10:33 -0400

    fixed first commit from ilab

--- memgrind.c
+++ memgrind.c
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-
 int main(int argc, char * argv[]) {
 	return 0;
-=======
-
-int main(int argc, char * argv[]) {
-	return 0;
->>>>>>> 824b14cbfd4bf3e5bc47725c4c0fb1ecb859379b
 }
--- mymalloc.c
+++ mymalloc.c
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 static char myblock[5000];
 
 int main(int argc, char * argv[]) {
 	
 	
 	return 0;
-=======
-static char myblock[5000];
-
-int main(int argc, char * argv[]) {
-	
-	
-	return 0;
->>>>>>> 824b14cbfd4bf3e5bc47725c4c0fb1ecb859379b
 }
