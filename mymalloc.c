611172836473d6e342d79b33c7f38a5a239aec3a
OmGiTzJake <jake@jakekarasik.com>
2016-09-27 18:38:58 -0400

    Add basic mymalloc and debugging functions

--- mymalloc.c
+++ mymalloc.c
@@ -1,9 +1,23 @@
 //Jake Karasik
 //Nicholas Petriello
-
-static char myblock[5000];
+#include "mymalloc.h"
 
 int main(int argc, char * argv[]) {
+	//test mymalloc of string
+	char * set_string = "hello";
+	char * a_string = mymalloc(strlen(set_string)+1);
+	strcpy(a_string,set_string);
+	printf("%s\n",a_string);
+	
+	//test mymalloc of int
+	int	set_int = 5;
+	int * a_int = mymalloc(sizeof(int));
+	*a_int = set_int;
+	printf("%d\n",*a_int);
+
+	//print view of memory and tag list
+	printMem(10);
+	printTags(10);
 
 	return 0;
 }
--- mymalloc.h
+++ mymalloc.h
@@ -1,16 +1,63 @@
-#define malloc(x) mymalloc(x, __FILE__, __LINE__)
-#define free(x) myfree(x, __FILE__, __LINE__)
+//#define malloc(x) mymalloc(x, __FILE__, __LINE__)
+//#define free(x) myfree(x, __FILE__, __LINE__)
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+//******Structs*******//
+typedef struct tag_ {
+	int is_set;
+	int size;
+	int index;
+} tag;
+
+//****Global Vars****//
+static char myblock[5000];
+static tag address_list[5000];
+static int current_index = 0;
 
 //*****Prototypes*****//
-char * malloc(int size);
-void free(char * ptr);
+void * mymalloc(int size);
+void myfree(char * ptr);
 
 //*****Functions*****//
-char * malloc(int size) {
-	printf("Memory of size %d allocated\n",size);
-	return 0x0;
+void * mymalloc(int size) {
+	tag new_tag;
+	new_tag.is_set = 1;
+	new_tag.size = size;
+	new_tag.index = current_index;
+	address_list[current_index] = new_tag;
+
+	if (current_index == 4999) {
+
+		current_index = 0;
+
+	} else {
+
+		current_index += size;
+
+	}
+
+	return &myblock[new_tag.index];
 }
 
-void free(char * ptr) {
+void myfree(char * ptr) {
 	
+}
+
+void printMem(int max_print_index) {
+	int i;
+	for (i=max_print_index;i>=0;i--) {
+		printf("myblock[%d]=%d\n",i,myblock[i]);
+	}
+}
+
+void printTags(int max_print_index) {
+	int i;
+	for (i=max_print_index;i>=0;i--) {
+		printf("address_list[%d]\n",i);
+		printf("\tis_set: %d\n", address_list[i].is_set);
+		printf("\tsize: %d\n", address_list[i].size);
+		printf("\tindex: %d\n", address_list[i].index);
+	}
 }
