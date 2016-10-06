8dc2f73f30b08f73bf3315d5f4ce2432afae1348
OmGiTzJake <jake@jakekarasik.com>
2016-10-05 21:59:14 -0400

    Redoing with meta info

--- mymalloc.c
+++ mymalloc.c
@@ -2,6 +2,65 @@
 //Nicholas Petriello
 #include "mymalloc.h"
 
+//****Global Vars****//
+static char myblock[5000];
+int current_index = 0;
+
+//*****Functions*****//
+int nextOpenSpot(size_t size) {
+
+	int i;
+	for (i=current_index;i<5000;i++) {
+		if () {
+			
+		}
+	}
+}
+
+void * mymalloc(size_t size) {
+	int return_address = &myblock[current_index];
+
+	
+
+	return return_address;
+}
+
+void myfree(void * ptr) {
+	int position = abs((char *)ptr - (char *)&myblock[0]);
+	int size = address_list[position].size;
+	int i;
+	for (i=0;i<size;i++) {
+		tag clean_tag = {0,0};
+		address_list[position+i] = clean_tag;
+	}
+}
+
+void printMem(int max_print_index) {
+	int i;
+	for (i=max_print_index;i>=0;i--) {
+		printf("myblock[%d]=%d\n",i,myblock[i]);
+	}
+}
+
+void printTags(int max_print_index) {
+	int i;
+	for (i=max_print_index;i>=0;i--) {
+		printf("address_list[%d]\n",i);
+		printf("\tis_set: %d\n", address_list[i].is_set);
+		printf("\tsize: %d\n", address_list[i].size);
+	}
+}
+
+void printArraysAsPicture(int max_print_index) {
+	printf("myblock:\taddress_list:\n");
+	int i;
+	for (i=max_print_index;i>=0;i--) {
+			printf("%5d[%d]\t",i,myblock[i]);
+			printf("%5d[%s]\n",i,address_list[i].is_set ? "x" : " ");
+	}
+}
+
+
 int main(int argc, char * argv[]) {
 	//test mymalloc of string
 	char * set_string = "hello";
@@ -16,11 +75,11 @@ int main(int argc, char * argv[]) {
 	printf("%d\n",*a_int);
 
 	//print view of memory and tag list
-	printArraysAsPicture(10);
-	myfree(a_string);
-	myfree(a_int);
-	printf("---------\n");
-	printArraysAsPicture(10);
+	///printArraysAsPicture(10);
+	//myfree(a_string);
+	//myfree(a_int);
+	//printf("---------\n");
+	//printArraysAsPicture(10);
 
 	return 0;
 }
--- mymalloc.h
+++ mymalloc.h
@@ -4,77 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
-//******Structs*******//
-typedef struct tag_ {
-	int is_set;
-	int size;
-} tag;
-
-//****Global Vars****//
-static char myblock[5000];
-static tag address_list[5000];
-static int current_index = 0;
-
 //*****Prototypes*****//
-void * mymalloc(int size);
+void * mymalloc(size_t size);
 void myfree(void * ptr);
-
-//*****Functions*****//
-void * mymalloc(int size) {
-	tag new_tag = {1, size};
-	int returnIndex = current_index;
-	address_list[current_index] = new_tag;
-
-	int i;
-	for (i=1;i<size;i++) {
-		tag spacer_tag = {1, size-i};
-		address_list[current_index+i] = spacer_tag;
-	}
-
-	if (current_index == 4999) {
-
-		current_index = 0;
-
-	} else {
-
-		current_index += size;
-
-	}
-
-	return &myblock[returnIndex];
-}
-
-void myfree(void * ptr) {
-	int position = abs((char *)ptr - (char *)&myblock[0]);
-	int size = address_list[position].size;
-	int i;
-	for (i=0;i<size;i++) {
-		tag clean_tag = {0,0};
-		address_list[position+i] = clean_tag;
-	}
-}
-
-void printMem(int max_print_index) {
-	int i;
-	for (i=max_print_index;i>=0;i--) {
-		printf("myblock[%d]=%d\n",i,myblock[i]);
-	}
-}
-
-void printTags(int max_print_index) {
-	int i;
-	for (i=max_print_index;i>=0;i--) {
-		printf("address_list[%d]\n",i);
-		printf("\tis_set: %d\n", address_list[i].is_set);
-		printf("\tsize: %d\n", address_list[i].size);
-	}
-}
-
-void printArraysAsPicture(int max_print_index) {
-	printf("myblock:\taddress_list:\n");
-	int i;
-	for (i=max_print_index;i>=0;i--) {
-			printf("%5d[%d]\t",i,myblock[i]);
-			printf("%5d[%s]\n",i,address_list[i].is_set ? "x" : " ");
-	}
-}
+int nextOpenSpot(size_t size);
